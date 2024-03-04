0992fdb0163822fb69b00b4594223f2b6bfbb86e
Alexander <117208524+PlatosCodes@users.noreply.github.com>
2024-03-04 11:35:50 -0500

    add production backend url to axios instance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import Cookie from 'js-cookie';
 
 const instance = Axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://api.alexandermerola.com/merolastation',
     withCredentials: true
 });
 
